1851c2784a894e915d4a6ab19276569ca125648d
Daniel Valencia <dvalencia@saatvamattress.com>
2022-09-11 20:06:22 -0500

    Add redux-persist to persist user information

--- src/index.js
+++ src/index.js
@@ -3,15 +3,28 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
-import 'semantic-ui-css/semantic.min.css';
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import "semantic-ui-css/semantic.min.css";
 import App from "./components/App";
 import reducers from "./reducers";
+import { PersistGate } from "redux-persist/integration/react";
 
-const store = createStore(reducers, applyMiddleware(thunk));
+const persistConfig = {
+  key: "root",
+  whitelist: ['user'],
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+const store = createStore(persistedReducer, applyMiddleware(thunk));
+const persistor = persistStore(store);
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.querySelector("#root")
 );
